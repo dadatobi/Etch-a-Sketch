6f47b1d57c58ac9a66a37a96f9025696a974b3b1
dadatobi <64893460+dadatobi@users.noreply.github.com>
2021-04-21 22:05:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,33 @@ const reset = document.querySelector('#reset');
 const changeGridSize = document.querySelector('#gridS');
 var divBoxes, grid;
 // create n*n no of div boxes
+
 grid = prompt("enter the number for your grid per side", " ");
 
-
-function createDivBox(grid){
-for (let i=0; i< grid*grid; i++){
-    const divElement = document.createElement('div');
-    divElement.classList.add('box');
-    gridContainer.style.gridTemplateColumns = `repeat(${grid}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${grid}, 1fr)`
-    container.appendChild(divElement);
+function creatGrid(grid) {
+    for (let i=0; i< grid*grid; i++){
+        const divElement = document.createElement('div');
+        divElement.classList.add('box');
+        gridContainer.style.gridTemplateColumns = `repeat(${grid}, 1fr)`
+        gridContainer.style.gridTemplateRows = `repeat(${grid}, 1fr)`
+        container.appendChild(divElement);
     };
     divBoxes = document.querySelectorAll('.box');
     divBoxes.forEach((box) => {
-        box.addEventListener('mouseover', function(){
-           if (grid<=100) {
-               box.classList.add('playing');
-            }else {
-                alert('enter a number not more than 100');
-                grid = prompt("enter the number for your grid per side", " ");
-            } 
-        });
-    });
+         box.addEventListener('mouseover', function() {
+              box.classList.add('playing'); 
+                });
+            });
+};
+
+function createDivBox(grid){
+    if (grid<=100) {
+        creatGrid(grid);
+    }else {
+    alert('enter a number not more than 100');
+    grid = prompt("enter the number for your grid per side", " ");
+    creatGrid(grid);
+    }
 };
 
 reset.addEventListener('click', function(){
@@ -36,11 +41,7 @@ reset.addEventListener('click', function(){
 
 changeGridSize.addEventListener('click', function(){
     window.location.reload();
+    
 });
 
 createDivBox(grid);
-
-
-
-
-
